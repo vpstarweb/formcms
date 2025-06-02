ea19077ddc0f6c3d4af3c3875ea8378ff0befd5a
vpstarweb <5259751+vpstarweb@users.noreply.github.com>
2025-06-02 11:44:32 -0400

    update examples to use package 0.5.2

--- examples/SqlServerDemo/SqlServerCmsApp/Program.cs
+++ examples/SqlServerDemo/SqlServerCmsApp/Program.cs
@@ -1,7 +1,7 @@
 using CmsApp;
 using FormCMS;
-using FormCMS.Auth;
 using FormCMS.Auth.Builders;
+using FormCMS.Auth.Models;
 using FormCMS.Utils.ResultExt;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -15,9 +15,10 @@ webBuilder.Services.AddSqlServerCms(connectionString);
 
 //add permission control service 
 webBuilder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
-webBuilder.Services.AddCmsAuth<IdentityUser, IdentityRole, AppDbContext>(new AuthConfig());
+webBuilder.Services.AddCmsAuth<CmsUser, IdentityRole, AppDbContext>(new AuthConfig());
 webBuilder.Services.AddAuditLog();
 webBuilder.Services.AddActivity();
+webBuilder.Services.AddComments();
 
 var webApp = webBuilder.Build();
 
